7df7a8e602b7374ec69a4f4731c79a3b4f409a92
Allan Nava <allannava95@gmail.com>
2023-08-30 15:35:14 +0200

    Update keycloak_security_manager.py

--- keycloak_security_manager.py
+++ keycloak_security_manager.py
@@ -35,7 +35,7 @@ class AuthOIDCView(AuthOIDView):
                 user = sm.add_user(info.get('preferred_username'), info.get('given_name', ''), info.get('family_name', ''),
                                    info.get('email'), [sm.find_role(role) for role in roles])
             # need to check if is it correct
-            user.is_active = True
+            setattr(user, "is_active", True)
             #
             login_user(user, remember=False)
             return redirect(self.appbuilder.get_url_for_index)
